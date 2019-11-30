781885996f1f8d5bab2b7d40359c12ce790a7fb6
Jakir Hossain <jakirseu@gmail.com>
2019-11-30 12:13:36 +0400

    update projects

--- app/src/main/java/me/jakir/androidradiobutton/MainActivity.java
+++ app/src/main/java/me/jakir/androidradiobutton/MainActivity.java
@@ -1,12 +1,13 @@
 package me.jakir.androidradiobutton;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
